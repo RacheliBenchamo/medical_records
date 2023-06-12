8c50458f1e3ae5532a7f49a2ec205f696ec79d78
Racheli <Racheli@DESKTOP-1GVI58U>
2023-06-12 12:02:20 +0300

    added more styles

--- react-client/src/components/Header.js
+++ react-client/src/components/Header.js
@@ -12,19 +12,8 @@ function Header() {
                     <span>Hygiene Hub</span>
                 </div>
                 <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                    <button
-                        className="navbar-toggler"
-                        type="button"
-                        data-bs-toggle="collapse"
-                        data-bs-target="#navbarNav"
-                        aria-controls="navbarNav"
-                        aria-expanded="false"
-                        aria-label="Toggle navigation"
-                    >
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
                     <div className="collapse navbar-collapse" id="navbarNav">
-                        <ul className="navbar-nav ml-auto">
+                        <ul className="navbar-nav">
                             <li className="nav-item">
                                 <Link className="nav-link" to="/records">
                                     Records
