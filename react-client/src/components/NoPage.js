e02f2a86aa0516a92220370121ed48694676a256
Racheli <Racheli@DESKTOP-1GVI58U>
2023-06-13 17:47:35 +0300

    Finished with NoPage style

--- react-client/src/components/NoPage.js
+++ react-client/src/components/NoPage.js
@@ -1,10 +1,15 @@
 import React from 'react';
-function NoPage() {
+import { Link } from 'react-router-dom';
 
+function NoPage() {
     return (
-        <>
-            <h2>NoPage</h2>
-        </>
+        <div className="container no-page-container">
+            <h2>No Page Found</h2>
+            <Link to="/" className="button">
+                Back to Home Page
+            </Link>
+        </div>
     );
 }
+
 export default NoPage;
